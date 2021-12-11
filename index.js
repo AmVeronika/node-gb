435b8f76c8ee74583000974373ccf99e6fdaf7ac
Veronika <mihotinaw@gmail.com>
2021-12-11 15:03:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const colors = require("colors/safe");
 const [firstValue, secondValue] = process.argv.slice(2);
 let arrOfPrime = [];
-let even = (el)=> parseFloat(el) == Number;
 
 if (!Number(firstValue)||!Number(secondValue)) {
     console.log(colors.red('Вы не ввели число'))
     process.exit(1);
 }
-selectionNumber:
-    for (let i = firstValue; i <= secondValue; i++) {
+function selectionNumber(a, b) {
+    for (let i = a; i <= b; i++) {
+        let flag = true
         for (let j = 2; j < i; j++) {
             if (i % j === 0) {
-                continue selectionNumber
+                flag = false
             }
         }
-        arrOfPrime.push(+i)
+        if(flag) {
+            arrOfPrime.push(+i)
+        }
     }
+}
+selectionNumber(firstValue, secondValue)
 
 if (+firstValue > +secondValue) {
     console.log(colors.red('Первое число не может быть больше торого'))
@@ -31,4 +35,3 @@ if (+firstValue > +secondValue) {
             console.log(colors.red(arrOfPrime[2 + 3 * i]))
     }
 }
-
